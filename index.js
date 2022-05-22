eb3d12f4c3f055f0808bbcafd6f5a68527b7ea29
shariarhossain23e <shariarhossain23@gamil.comm>
2022-05-22 10:40:50 +0600

    specific product

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const cors = require("cors")
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require("dotenv").config()
 const app = express()
 const jwt = require('jsonwebtoken');
@@ -9,7 +9,6 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors())
 app.use(express.json())
-
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.52sp4.mongodb.net/?retryWrites=true&w=majority`;
@@ -25,6 +24,15 @@ async function run() {
     // all parts
     app.get('/parts',async(req,res)=>{
       const result = await carShop.find().toArray()
+      res.send(result)
+    })
+
+    // get specific user
+    app.get('/parts/:id',async(req,res)=>{
+      const id = req.params.id;
+      console.log(id);
+      const filter = {_id:ObjectId(id)};
+      const result = await carShop.findOne(filter);
       res.send(result)
     })
 
